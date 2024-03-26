62e08cd8eeadb013c2f9a9c149f5dd2d59f2b379
Teti <rohatetiana@gmail.com>
2024-03-26 15:59:56 +0100

    server migration to ts

---
+++ src/server/models/User.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
